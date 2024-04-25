fd46b859459ddfdd10c42824a7b6be12a8820db2
bobykincompany <126823182+bobykincompany@users.noreply.github.com>
2024-04-25 11:41:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+# Первые шаги
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Шаг 1 импорт FastAPI
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Шаг 2 создаем экземпляр FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Шаг 3 определение операции пути
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Шаг 4  определение функции операции пути
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Шаг 5  возвращаем результат
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
